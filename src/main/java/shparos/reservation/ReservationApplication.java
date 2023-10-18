855c99e43caeeac0c88da1c02844b8a8d269a4d3
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-10-18 10:23:57 +0900

    reservation 레포지터리 첫 업로드

---
+++ src/main/java/shparos/reservation/ReservationApplication.java
@@ -0,0 +1,16 @@
+package shparos.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+}
