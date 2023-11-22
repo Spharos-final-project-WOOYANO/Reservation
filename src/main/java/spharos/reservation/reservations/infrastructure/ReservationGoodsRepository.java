85c584b45f54a6a224fb1caa599edde9e3c45b4f
ASY <showasy@naver.com>
2023-11-22 10:37:14 +0900

    Feat 서비스 상품 등록, 조회 삭제 기능 추가

--- src/main/java/spharos/reservation/reservations/infrastructure/ReservationGoodsRepository.java
+++ src/main/java/spharos/reservation/reservations/infrastructure/ReservationGoodsRepository.java
@@ -3,9 +3,12 @@ package spharos.reservation.reservations.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spharos.reservation.reservations.domain.ReservationGoods;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReservationGoodsRepository extends JpaRepository<ReservationGoods, Long> {
 
     Optional<ReservationGoods> findById(Long id);
+    List<ReservationGoods> findByServiceIdOrderByIdDesc(Long serviceId);
+
 }
